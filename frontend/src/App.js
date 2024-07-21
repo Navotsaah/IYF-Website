eea3573f879bf1c17b21f7b9981632e3f8ecc769
krishnendusukumar <krishnendu.1923co1112@kiet.edu>
2024-07-21 13:31:26 +0530

    dynamic frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './assets/css/App.css';
 import Navbar from './views/components/Navbar';
 import Header from './views/pages/Main';
-import Feature from './Feature';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
-      <Feature />
     </div>
   );
 }
